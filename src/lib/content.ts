71ab1f21014e734e1ca271698c21f362f18660ee
errollvanmaramba <erroll.maramba04@gmail.com>
2026-03-10 13:18:40 +0800

    Delete src directory

--- src/lib/content.ts
+++
@@ -1,6 +0,0 @@
-import projects from "../content/projects.generated.json";
-import type { Project } from "../types/content";
-
-export const allProjects = projects as Project[];
-
-export const featuredProjects = allProjects.filter((project) => project.featured);
